6b335916a9e6e9e0bb2161e07c5bf4d1f5628298
ArathG <agonzalez.airsoft@outlook.com>
2018-12-28 20:41:22 -0600

    REG-002
    Form

--- vistas/companies.php
+++ vistas/companies.php
@@ -40,7 +40,96 @@
               </div>
           </div>
       </section>
+    </div>
 
+    <div id="companyModal" class="modal fade">
+        <div class="modal-dialog">
+            <form method="post" id="companyForm">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Agregar Empresa</h4>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                <label for="name">Nombre:</label>
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Nombre">
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                <label for="rfc">RFC:</label>
+                                <input type="text" class="form-control" id="rfc" name="rfc" placeholder="RFC">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-5">
+                                <label for="street">Calle:</label>
+                                <input type="text" class="form-control" id="street" name="street" placeholder="Calle">
+                            </div>
+
+                            <div class="form-group col-md-2">
+                                <label for="internal_number">N. Interno:</label>
+                                <input type="text" class="form-control" id="internal_number" name="internal_number" placeholder="N.I">
+                            </div>
+
+                            <div class="form-group col-md-2">
+                                <label for="external_number">N. Externo:</label>
+                                <input type="text" class="form-control" id="external_number" name="external_number" placeholder="N.E">
+                            </div>
+
+                            <div class="form-group col-md-3">
+                                <label for="colony">N. Externo:</label>
+                                <input type="text" class="form-control" id="colony" name="colony" placeholder="Colonia">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-4">
+                                <label for="municipality">Municipio:</label>
+                                <input type="text" class="form-control" id="municipality" name="municipality" placeholder="Municipio">
+                            </div>
+
+                            <div class="form-group col-md-2">
+                                <label for="zip_code">C.P:</label>
+                                <input type="text" class="form-control" id="zip_code" name="zip_code" placeholder="CP">
+                            </div>
+
+                            <div class="form-group col-md-3">
+                                <label for="state">Estado:</label>
+                                <input type="text" class="form-control" id="state" name="state" placeholder="Estado">
+                            </div>
+
+                            <div class="form-group col-md-3">
+                                <label for="country">Pais:</label>
+                                <input type="text" class="form-control" id="country" name="country" placeholder="Pais">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="mail1">Correo 1:</label>
+                            <input type="text" class="form-control" id="mail1" name="mail1" placeholder="example@example.com">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="mail2">Correo 2:</label>
+                            <input type="text" class="form-control" id="mail2" name="mail2" placeholder="example@example.com">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="mail3">Correo 3:</label>
+                            <input type="text" class="form-control" id="mail3" name="mail3" placeholder="example@example.com">
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+                        <button type="button" class="btn btn-success">Guardar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
 
 <?php
